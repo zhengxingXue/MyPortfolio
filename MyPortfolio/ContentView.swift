b8318479f586b7ac39964cea1f12109fd3efecef
zhengxingXue <zx252cornell@gmail.com>
2021-09-02 13:06:53 -0400

    Initial Commit

---
+++ MyPortfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyPortfolio
+//
+//  Created by Jim's MacBook Pro on 9/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyPortfolio/MyPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPortfolioApp.swift
+//  MyPortfolio
+//
+//  Created by Jim's MacBook Pro on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MyPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
